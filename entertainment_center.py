c17a7fdaf025b128b97012adc4f65bda2ab2fb6e
Gustavo Paes <gustavo.paes@gmail.com>
2016-07-11 20:38:00 -0300

    applying continuation line formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,20 @@
 from media import Movie, TvShow
 import fresh_tomatoes
 
-toy_story = Movie("tt0114709", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-elit_squad = Movie("tt0861739", "https://www.youtube.com/watch?v=_gnJB10WTpE")
-pulp_fiction = Movie("tt0110912", "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+toy_story = Movie("tt0114709",
+                  "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-friends = TvShow("tt0108778", "https://www.youtube.com/watch?v=hDNNmeeJs1Q")
-sopranos = TvShow("tt0141842", "https://www.youtube.com/watch?v=ch-X-9J73aQ")
+elit_squad = Movie("tt0861739",
+                   "https://www.youtube.com/watch?v=_gnJB10WTpE")
+
+pulp_fiction = Movie("tt0110912",
+                     "https://www.youtube.com/watch?v=s7EdQ4FqbhY")
+
+friends = TvShow("tt0108778",
+                 "https://www.youtube.com/watch?v=hDNNmeeJs1Q")
+
+sopranos = TvShow("tt0141842",
+                  "https://www.youtube.com/watch?v=ch-X-9J73aQ")
 
 movies = [toy_story, elit_squad, pulp_fiction]
 series = [sopranos, friends]
